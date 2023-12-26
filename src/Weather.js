c291da3410fd7689fff6b0ebf08afe1123c18518
Akangbe Kafilat Bisola <ojuawobisola25@gmail.com>
2023-12-26 22:49:33 +0100

    initial commit

--- src/Weather.js
+++ src/Weather.js
@@ -32,9 +32,9 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "001bc651977f4b024af4d84282b0f02a";
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+      axios.get(apiUrl).then(handleResponse);
   }
 
   if (weatherData.ready) {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "001bc651977f4b024af4d84282b0f02a";
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
